64cd9fcc5fed9e51d610db0b6332b30fb7c2648b
Ocean <dvsocean@icloud.com>
2018-08-10 15:15:45 -0700

    progress

---
+++ src/main/java/com/cisClient/cisclient/ProductController.java
@@ -0,0 +1,40 @@
+package com.cisClient.cisclient;
+
+import java.net.URI;
+import java.util.HashMap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ProductController {
+
+  @Bean
+  private RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  @GetMapping("/find/{id}")
+  public String verifyProduct(@PathVariable("id") int id){
+    Product pr = restTemplate().getForObject("http://localhost:5000/find/" + id, Product.class);
+    return pr.getName();
+  }
+
+  @PostMapping("/describe")
+  public Product locateAndDescribe(@RequestParam("name") String name){
+    HttpEntity<Product> request = new HttpEntity<>(new Product());
+    Product pr = restTemplate().postForObject("http://localhost:5000/describe", request, Product.class);
+    return pr;
+//    ResponseEntity<Product> res = restTemplate().exchange(RequestEntity.post(URI.create("http://localhost:5000/describe"))
+//    .contentType(MediaType.APPLICATION_JSON).body(product), Product.class);
+//    return res.getBody();
+  }
+}
---
+++ src/test/java/com/cisClient/cisclient/ProductControllerTests.java
@@ -0,0 +1,45 @@
+package com.cisClient.cisclient;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(ids = "com.producer:cis-producer:+:stubs:5000", stubsMode = StubsMode.LOCAL)
+public class ProductControllerTests {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void shouldReturnMongoose() throws Exception {
+    mockMvc.perform(get("/find/1")).andExpect(status().isOk())
+        .andExpect(jsonPath("$").value("Mongoose"));
+  }
+
+  @Test
+  public void verifyObject() throws Exception {
+    mockMvc.perform(post("/describe").contentType(MediaType.APPLICATION_JSON)
+    .param("name", "Playstation").param("price", "120"))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+}
